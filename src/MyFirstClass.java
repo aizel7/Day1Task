4053cacbdfb30ac899b2c2f179898e05f20a681c
aizel7 <aizelvali@gmail.com>
2022-05-14 00:38:12 +0300

    Initial commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,21 @@
+public class MyFirstClass  {
+    public static void main(String[] args) {
+
+        byte a = 5;
+        short b = 13;
+        int c = 35;
+        long d = 123456789;
+        float e = 25.767F;
+        double f = 26.22;
+        boolean yesno = true;
+        char g = 'A';
+        System.out.println (a);
+        System.out.println (b);
+        System.out.println (c);
+        System.out.println (d);
+        System.out.println (e);
+        System.out.println (f);
+        System.out.println (yesno);
+        System.out.println (g);
+    }}
+   
